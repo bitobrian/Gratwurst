621f897665fc026a07db38a21ddf84dc30f1c4d2
jimeherr <james-herr@sbcglobal.net>
2026-02-28 11:19:09 -0600

    added combat check

--- Gratwurst.lua
+++ Gratwurst.lua
@@ -465,7 +465,7 @@ function GuildAchievementMessageEventReceived(isDebug, author)
 
 	local canGrats = false
 	local random = math.random(1, 100)
-	if random <= GratwurstVariancePercentage then
+	if random <= GratwurstVariancePercentage and not InCombatLockdown() then
 		canGrats = true
 	end
 	GratwurstDelayInSeconds = math.random(1,GratwurstRandomDelayMax)
